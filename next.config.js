479b88c09250b5f1f015b6f53b26352b28ebc83b
Malitha Sandaruwan <sandaruwanmalitha2018@gmail.com>
2023-04-19 07:04:46 +0530

    05. Add SignIn and SignUp UI page & install react-hook-form

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["image.tmdb.org"],
+    domains: ["rb.gy", "image.tmdb.org", "res.cloudinary.com"],
   },
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -31,6 +31,7 @@ export default function Home({
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
