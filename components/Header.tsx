a4cb355b88bbba8f3eea2854869e67ccd89649c5
Malitha Sandaruwan <sandaruwanmalitha2018@gmail.com>
2023-06-28 10:44:29 +0530

    16. Added dropdown menu in mobile screens

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import useAuth from "@/hooks/useAuth";
 import { BellIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import BasicMenu from "./BasicMenu";
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -26,33 +27,35 @@ const Header = () => {
 
   return (
     <header className={`${isScrolled && "bg-[#141414]"}`}>
-      <div className="flex items-center space-x-2 md:space-x-10">
+      <div className='flex items-center space-x-2 md:space-x-10'>
         <img
-          src="https://rb.gy/ulxxee"
+          src='https://rb.gy/ulxxee'
           width={100}
           height={100}
-          alt="netflix-logo"
-          className="cursor-pointer object-contain"
+          alt='netflix-logo'
+          className='cursor-pointer object-contain'
         />
 
-        <ul className="hidden space-x-4 md:flex">
-          <li className="headerLink">Home</li>
-          <li className="headerLink">TV Shows</li>
-          <li className="headerLink">Movies</li>
-          <li className="headerLink">New & Popular</li>
-          <li className="headerLink">My List</li>
+        <BasicMenu />
+
+        <ul className='hidden space-x-4 md:flex'>
+          <li className='headerLink'>Home</li>
+          <li className='headerLink'>TV Shows</li>
+          <li className='headerLink'>Movies</li>
+          <li className='headerLink'>New & Popular</li>
+          <li className='headerLink'>My List</li>
         </ul>
       </div>
 
-      <div className="flex items-center space-x-4 text-sm font-[400]">
-        <MagnifyingGlassIcon className="hidden h-6 w-6 sm:inline" />
-        <p className="hidden lg:inline">Kids</p>
-        <BellIcon className="h-6 w-6" />
-        <Link href="/account">
+      <div className='flex items-center space-x-4 text-sm font-[400]'>
+        <MagnifyingGlassIcon className='hidden h-6 w-6 sm:inline' />
+        <p className='hidden lg:inline'>Kids</p>
+        <BellIcon className='h-6 w-6' />
+        <Link href='/account'>
           <img
-            src="https://rb.gy/g1pwyx"
-            alt=""
-            className="cursor-pointer rounded"
+            src='https://rb.gy/g1pwyx'
+            alt=''
+            className='cursor-pointer rounded'
           />
         </Link>
       </div>
